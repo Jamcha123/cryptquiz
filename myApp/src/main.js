cbf34c348a9396004b95be05238ea6d733f65e0f
james <90643139+Jamcha123@users.noreply.github.com>
2025-03-11 13:00:56 +0100

    Delete myApp directory

--- myApp/src/main.js
+++
@@ -1,8 +0,0 @@
-import './style.css'
-import fs from 'fs'
-import {} from 'crypto'; 
-import axios from 'axios'
-import * as cheerio from 'cheerio'
-
-
-const types = ["MD5", "SHA1", "SHA256", "SHA512", "SHA224", "SHA384"]
